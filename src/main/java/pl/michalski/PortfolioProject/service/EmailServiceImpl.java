cc707a03da0428a185362c2d6c8294c6c64f5d16
pmichalski <p.michalski889@interia.pl>
2020-09-27 22:46:26 +0200

    Update EmailServiceImpl.java

--- src/main/java/pl/michalski/PortfolioProject/service/EmailServiceImpl.java
+++ src/main/java/pl/michalski/PortfolioProject/service/EmailServiceImpl.java
@@ -28,8 +28,8 @@ public class EmailServiceImpl implements EmailService{
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mail, true);
             helper.setTo(to);
-            helper.setReplyTo("porfolioprojectmailsender@gmail.com");
-            helper.setFrom("porfolioprojectmailsender@gmail.com");
+            helper.setReplyTo("email");
+            helper.setFrom("email");
             helper.setSubject(title);
             helper.setText(content, true);
         } catch (MessagingException e) {
